404a09b3a1a5e716dd55408590da5fa19adb834e
saidechlahbi <echlahbisaid@gmail.com>
2024-07-14 06:16:24 -0700

    hh

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('loginBtn').addEventListener('click', function() {
+    alert('Login button clicked!');
+});
+
+document.getElementById('signupBtn').addEventListener('click', function() {
+    alert('Signup button clicked!');
+});
+
